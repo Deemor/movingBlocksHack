ec26dcbcd5307bbeb61125bed439abc419c17821
Deemor <90990679+Deemor@users.noreply.github.com>
2022-02-23 23:42:56 +0100

    Update UD 5City
    Dodano hack z 5City

--- src/movingBlocks.js
+++ src/movingBlocks.js
@@ -9,6 +9,24 @@ const CLICKED_IF_MORE_THAN_ONE = '#30475d'
 const COLOR_IF_ONE = '#1e3955'
 const CLICKED_IF_ONE = '#30475d'
 
+const COLOR_IF_ONE_5CITY = '#005c53'
+const COLOR_IF_MORE_THAN_ONE_5CITY = ['#005c53','#1c8b80','#0850ad','#09ad21','#076905','#12e5de']
+const CLICKED_IF_5CITY = '#a0c131'
+
+const SQUARE_WIDTH_5CITY = "80px";
+const SQUARE_WIDTH_NOPIXEL = "100px";
+
+const SQUARE_WIDTH_5CITY_INT = 80;
+const SQUARE_WIDTH_NOPIXEL_INT = 100;
+
+const SQUARE_FONT_5CITY = "50px"
+const SQUARE_FONT_COLOR_5CITY = "#d7ef4f"
+const SQUARE_FONT_WEIGHT_5CITY = "bold"
+
+const SQUARE_FONT_NOPIXEL = "60px"
+const SQUARE_FONT_COLOR_NOPIXEL = "white"
+const SQUARE_FONT_WEIGHT_NOPIXEL = "normal"
+
 
 
 function playSound(name, volume){
@@ -23,51 +41,91 @@ var squareToClick = 0;
 var amountOfColors = 1;
 var isClickable = false;
 var result = false;
-var timeToRemember = 0;
+var timeToRemember = 4;
 var isBorder = 0;
 var main_size = 0;
+var type = "NOPIXEL";
+var time_5City = 1;
 
 var main_width = [1150, 1540]
 var main_height = [650 , 870]
 
-function getColorOfSquare(){
+function getColorOfSquareNOPIXEL(){
   if(amountOfColors == 1){
     return COLOR_IF_ONE;
   }else{
     return COLORS_IF_MORE_THAN_ONE[randomInt(amountOfColors)];
   }
 }
+function getColorOfSquare5City(){
+  if(amountOfColors == 1){
+    return COLOR_IF_ONE_5CITY;
+  }else{
+    return COLOR_IF_MORE_THAN_ONE_5CITY[randomInt(amountOfColors)];
+  }
+}
+
 
 function getColorOfSquareAfterClick(){
   if(amountOfColors == 1){
-    return CLICKED_IF_ONE;
+    return COLOR_IF_ONE_5CITY;
   }else{
     return CLICKED_IF_MORE_THAN_ONE;
   }
 }
 function makeNewPosition(){
     
-  // Get viewport dimensions (remove the dimension of the div)
-  var height = $("#main").height() - 100;
-  var width = $("#main").width() - 100;
+  if(type=="5CITY")
+  {
+    var height = $("#main5CityBox").height() - SQUARE_WIDTH_5CITY_INT;
+    var width = $("#main5CityBox").width() - SQUARE_WIDTH_5CITY_INT;
+    
+    var newHeight = $('#main5CityBox').offset().top + Math.floor(Math.random() * height);
+    var newWidth = $('#main5CityBox').offset().left + Math.floor(Math.random() * width);
+    
+    return [newHeight,newWidth];    
+  }else if(type=="NOPIXEL")
+  {
+    var height = $("#main").height() - SQUARE_WIDTH_NOPIXEL_INT;
+    var width = $("#main").width() - SQUARE_WIDTH_NOPIXEL_INT;
+    
+    var newHeight = $('#main').offset().top + Math.floor(Math.random() * height);
+    var newWidth = $('#main').offset().left + Math.floor(Math.random() * width);
   
-  var newHeight = $('#main').offset().top + Math.floor(Math.random() * height);
-  var newWidth = $('#main').offset().left + Math.floor(Math.random() * width);
-  
-  return [newHeight,newWidth];    
-  
+    return [newHeight,newWidth];    
+  }
+  return [0,0]
 }
 
 function animateDiv(divID){
   var newPosition = makeNewPosition();
   var oldPosition = $("#s"+String(divID)).offset();
   var speed = calcSpeed([oldPosition.top, oldPosition.left], newPosition);
-  
   $("#s"+String(divID)).animate({ top: newPosition[0], left: newPosition[1] }, speed, function(){
     animateDiv(divID);        
   });
   
 };
+function animateDiv5City(){
+  var speed = time_5City*1000;
+  for(var i = 1; i <= amountOfSquares - 1; i ++)
+  {
+    var newPosition = makeNewPosition();
+    $("#s"+String(i)).animate({ top: newPosition[0], left: newPosition[1] }, speed, function(){
+              
+    });
+  }
+  var newPosition = makeNewPosition();
+  $("#s"+String(i)).animate({ top: newPosition[0], left: newPosition[1] }, speed, function(){
+    animateDiv5City();
+  });
+
+  
+};
+async function stopProgressBar()
+{
+  $("#progress-bar").stop(true,true);
+}
 
 function calcSpeed(prev, next) {
   
@@ -90,7 +148,14 @@ function squareClick(divID)
     if(divID == squareToClick)
     {
       squareToClick+=1;
-      $("#s"+String(divID)).css("background-color",getColorOfSquareAfterClick())
+      
+      if(type=="5CITY")
+      {
+        $("#s"+String(divID)).css("background-color", CLICKED_IF_5CITY)
+      }else if(type=="NOPIXEL")
+      {
+        $("#s"+String(divID)).css("background-color",getColorOfSquareAfterClick())
+      }
       $("#s"+String(divID)).css("border","0px solid white")
       if(divID == amountOfSquares)
       {
@@ -120,19 +185,8 @@ function hideNumbers()
     $("#s"+String(i)).html("");
   }
 }
-async function startHack()
+async function startNopixelHack()
 {
-  COLORS_IF_MORE_THAN_ONE = shuffleArray(COLORS_IF_MORE_THAN_ONE)
-  result = false;
-  isClickable = false;
-  squareToClick = 1;
-  speedAmount =  document.getElementById("speed").value;
-  amountOfSquares = document.getElementById("amount").value;
-  timeToRemember = document.getElementById("timeToRemember").value;
-  amountOfColors =  document.getElementById("colors").value;
-  isBorder =  document.getElementById("border").value;
-  main_size = document.getElementById("mainSize").value;
-
   $("#main").height(main_height[main_size])
   $("#main").width(main_width[main_size])
 
@@ -142,7 +196,12 @@ async function startHack()
     var newPosition = makeNewPosition();
     $("#s"+String(i)).css("top",String(newPosition[0])+"px");
     $("#s"+String(i)).css("left",String(newPosition[1])+"px");
-    $("#s"+String(i)).css("background-color", getColorOfSquare())
+    $("#s"+String(i)).css("font-size",SQUARE_FONT_NOPIXEL);
+    $("#s"+String(i)).css("color", SQUARE_FONT_COLOR_NOPIXEL);
+    $("#s"+String(i)).css("font-weight", SQUARE_FONT_WEIGHT_NOPIXEL);
+    $("#s"+String(i)).css("background-color", getColorOfSquareNOPIXEL())
+    $("#s"+String(i)).css("width", SQUARE_WIDTH_NOPIXEL)
+    $("#s"+String(i)).css("height", SQUARE_WIDTH_NOPIXEL)
     if(isBorder == 1){
       $("#s"+String(i)).css("border", "1px solid white")
     }
@@ -162,14 +221,108 @@ async function startHack()
     });
   });
 }
+
+async function start5CityHack()
+{
+  for(var i = 1; i <= amountOfSquares; i ++)
+  {
+    $("#main5CityBox").append(`<div class="square noselect" id="s`+String(i)+`" onclick="squareClick(`+String(i)+`);"> </div>`);
+    var newPosition = makeNewPosition();
+    $("#s"+String(i)).css("top",String(newPosition[0])+"px");
+    $("#s"+String(i)).css("left",String(newPosition[1])+"px");
+    $("#s"+String(i)).css("font-size",SQUARE_FONT_5CITY);
+    $("#s"+String(i)).css("color", SQUARE_FONT_COLOR_5CITY);
+    $("#s"+String(i)).css("font-weight", SQUARE_FONT_WEIGHT_5CITY);
+    $("#s"+String(i)).css("background-color", getColorOfSquare5City())
+    $("#s"+String(i)).css("width", SQUARE_WIDTH_5CITY)
+    $("#s"+String(i)).css("height", SQUARE_WIDTH_5CITY)
+    if(isBorder == 1){
+      $("#s"+String(i)).css("border", "1px solid #ccec90")
+    }
+  }
+  animateDiv5City();
+  
+  showNumbers();
+  
+  var progBar = $("#progress-bar");
+  progBar.css("width","100%");
+  progBar.animate({
+    width: "0px"
+  }, {
+    duration: timeToRemember*1000,
+    ease: "linear",
+  });
+  await delay(timeToRemember);
+  await stopProgressBar();
+
+  hideNumbers();
+  isClickable = true;
+  
+  return new Promise(async (output) => {
+    $('body').on('DOMSubtreeModified', '#updateDiv', function(){
+      output(result);
+    });
+  });
+}
+async function startHack()
+{
+  COLORS_IF_MORE_THAN_ONE = shuffleArray(COLORS_IF_MORE_THAN_ONE)
+  result = false;
+  isClickable = false;
+  squareToClick = 1;
+  speedAmount =  document.getElementById("speed").value;
+  amountOfSquares = document.getElementById("amount").value;
+  timeToRemember = document.getElementById("timeToRemember").value;
+  amountOfColors =  document.getElementById("colors").value;
+  isBorder =  document.getElementById("border").value;
+  main_size = document.getElementById("mainSize").value;
+  time_5City = document.getElementById("time5CITY").value;
+
+  if(type=="5CITY")
+  {
+    var result = await start5CityHack();
+    $('#main5CityBox').empty();
+    if(result==true){
+      $("#main5CityBox").append(`<div style="width:100%; text-align: center; height:60px; margin-left:auto;margin-right:auto; font-size:60px; color:#d7ef4f; font-weight:bold;" >ACCESS GRANTED</div>`);
+    }else{
+      $("#main5CityBox").append(`<div style="width:100%; text-align: center; height:60px; margin-left:auto;margin-right:auto; font-size:60px; color:#d7ef4f; font-weight:bold;" >ACCESS DENIED</div>`);
+    }
+    
+    var progBar = $("#progress-bar");
+    progBar.css("width","100%");
+    progBar.animate({
+      width: "0px"
+    }, {
+      duration: 3*1000,
+      ease: "linear",
+    });
+    await delay(timeToRemember);
+    await stopProgressBar();
+    return result;
+  }else if(type=="NOPIXEL")
+  {
+    return await startNopixelHack();
+  }
+  
+}
 async function start(){
+  type = document.getElementById("type").value == 0? "NOPIXEL": "5CITY";;
+
   $('#main').empty();
+  $('#main5CityBox').empty();
   $("#settings").hide();
-  $("#main").show();
+  if(type=="5CITY")
+  {
+    $("#main5City").show();
+  }else if(type=="NOPIXEL")
+  {
+    $("#main").show();
+  }
   var result = await startHack();
   result == true ? $("#buttonStart").html("Success. Try again") : $("#buttonStart").html("Failed. Try again");
   $("#settings").show();
   $("#main").hide();
+  $('#main5City').hide();
 }
 $("#buttonStart").on( "click", function() {
   start();
